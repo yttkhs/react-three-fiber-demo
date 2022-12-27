08b9335fac6303b33b291b3be4876af2aedb53f1
Yuta Takahashi <takahashi@101de-sign.com>
2022-12-27 19:01:09 +0900

    feat: reset component and add types

--- src/pages/blob.tsx
+++ src/pages/blob.tsx
@@ -1,17 +1,8 @@
 import dynamic from 'next/dynamic'
-import Instructions from '@/components/dom/Instructions'
 
 const Blob = dynamic(() => import('@/components/canvas/Blob'), { ssr: false })
 
-export default function Page(props) {
-  return (
-    <Instructions>
-      This is the <span className='text-green-200'>/blob</span> route. Click on the blob to navigate back. The canvas was
-      not unmounted between route changes, only its contents. If you want scene contents to persist, put them into{' '}
-      <span className='text-green-200'>@/components/canvas/Scene</span>.
-    </Instructions>
-  )
-}
+export default function Page(props) {}
 
 Page.canvas = (props) => <Blob route='/' position-y={-0.75} />
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import Instructions from '@/components/dom/Instructions'
 
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
@@ -7,20 +6,12 @@ import Instructions from '@/components/dom/Instructions'
 // https://github.com/pmndrs/react-three-next/issues/49
 const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
 
-// Dom components go here
-export default function Page(props) {
-  return (
-    <Instructions>
-      This is a minimal starter for Nextjs + React-three-fiber and Threejs. Click on the{' '}
-      <span className='text-cyan-200'>atoms nucleus</span> to navigate to the{' '}
-      <span className='text-green-200'>/blob</span> page. OrbitControls are enabled by default.
-    </Instructions>
-  )
-}
+// Dom's components go here
+export default function Page(props) {}
 
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Logo scale={0.5} route='/blob' position-y={-1} />
+Page.canvas = (props) => <Logo scale={0.5} route='/blob' />
 
 export async function getStaticProps() {
   return { props: { title: 'Index' } }
