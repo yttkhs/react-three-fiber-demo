a911d12cb3da654fe97d8ee65459d00594c3a272
Yuta Takahashi <takahashi@101de-sign.com>
2023-01-31 19:13:44 +0900

    feat: add useFrame

--- src/components/canvas/Edit.tsx
+++ src/components/canvas/Edit.tsx
@@ -1,9 +1,11 @@
-import React, { useMemo } from 'react'
-import { useLoader } from '@react-three/fiber'
+import React, {useMemo, useRef} from 'react'
+import { useFrame, useLoader } from '@react-three/fiber'
 import { Center } from '@react-three/drei'
 import { SVGLoader } from 'three-stdlib'
+import {Group} from "three";
 
 export default function Edit({ route, ...props }) {
+  const group = useRef<Group>(null)
   const data = useLoader(SVGLoader, 'http://localhost:3030/img/logo.svg')
 
   const shapes = useMemo(() => {
@@ -18,8 +20,16 @@ export default function Edit({ route, ...props }) {
     })
   }, [data])
 
+  useFrame(({ clock }, delta, frame) => {
+    const t = clock.getElapsedTime();
+
+    // group.current.rotation.y = Math.sin(t) * (Math.PI / 8)
+    // group.current.rotation.x = Math.cos(t) * (Math.PI / 8)
+    group.current.rotation.z -= delta / 4
+  })
+
   return (
-    <Center>
+    <Center ref={group}>
       <group rotation={[0, 0, Math.PI / 2]}>
         {shapes.map((item) => (
           <Shape key={item.shape.uuid} {...item} />
@@ -32,7 +42,7 @@ export default function Edit({ route, ...props }) {
 function Shape({ shape, color }) {
   return (
     <mesh position={[0, 0, 0]} scale={0.01}>
-      <meshPhongMaterial attach='material' color={color} />
+      <meshStandardMaterial attach='material' color={color} roughness={0.5} vertexColors={true} />
       <extrudeGeometry attach='geometry' args={[shape, { depth: 100 }]} />
     </mesh>
   )
